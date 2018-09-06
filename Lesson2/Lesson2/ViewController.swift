7df06999dee3697bb897614d74a9aff64bc3b72f
kenkriaa <36570600+kenkriaa@users.noreply.github.com>
2018-09-06 19:20:48 +0200

    Add files via upload

---
+++ Lesson2/Lesson2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Kristòf Kenyeres on 2018. 09. 05..
+//  Copyright © 2018. Kristòf Kenyeres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
